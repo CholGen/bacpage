3318eb34e140f4c40b6c8d0feb4204e8357ce102
nate <snowboardman007@gmail.com>
2023-10-31 15:41:52 -0400

    cleaned up directories

--- workflow/gui/messages.py
+++
@@ -1,3 +0,0 @@
-DEFAULT_INFORMATION = "Greetings from Eureka. Select any tab on the left to edit parameters for that step. Hoverover any" \
-                      " tab or parameter and a description will appear here."
-MEAN_MESSAGE = "How about you go and fuck off."
---
+++ workflow/src/profiling.py
@@ -0,0 +1,9 @@
+# Identify reference genes
+# print( "Identifying gene sequences for typing...", end="" )
+# try:
+#    GENES = get_genes( config["reference_genes"] )
+# except Exception:
+#    print( "Error" )
+#    raise
+# print( "Done" )
+# print( f"The following genes will be used: [{', '.join( GENES.keys() )}]\n" )
